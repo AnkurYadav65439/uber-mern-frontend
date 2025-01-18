4cdef81c77aea0ec6e5d6033180d979809b4e14b
AnkurYadav65439 <ay0695617@gmail.com>
2025-01-18 16:18:41 +0530

    minor bug i.e.axios error fixed

--- src/pages/UserLogin.jsx
+++ src/pages/UserLogin.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom'
-import { useUserContext } from '../context/userContext';
+import { useUserContext } from '../context/userContext'
+import axios from 'axios'
 
 const UserLogin = () => {
 
